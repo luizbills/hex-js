f46036ca347fd54261ab8d506fa0cc28cc5952ef
Luiz "Bills <luizbills@pm.me>
2021-10-29 12:13:37 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// based on: https://stackoverflow.com/a/21648161
+
+export const hex_encode = function (str) {
+    var hex, i;
+    var result = "";
+    for (i=0; i<str.length; i++) {
+        hex = str.charCodeAt(i).toString(16);
+        result += ("000"+hex).slice(-4);
+    }
+    return result
+}
+
+export const hex_decode = function (str) {
+    var j;
+    var hexes = str.match(/.{1,4}/g) || [];
+    var back = "";
+    for(j = 0; j<hexes.length; j++) {
+        back += String.fromCharCode(parseInt(hexes[j], 16));
+    }
+    return back;
+}
